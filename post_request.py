fae08759f0fa8c32233602608e5d3324d6964cde
Nadav Ben Jakov <nadav@solebitlabs.com>
2017-03-19 19:41:54 +0200

    * Created a small script to receive all of the Hebrew stories in the library.  ** Important note: ALWAYS add the user-agent field in http requests or you WILL GET BANNED!!! * Added the output list to be analyzed offline

---
+++ post_request.py
@@ -0,0 +1,64 @@
+import pycurl
+from io import BytesIO
+
+
+
+def urlget(header, response):
+    print('Get function start')
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.URL, "http://www.shortstoryproject.com/he")
+    curl.setopt(pycurl.USERAGENT, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+    curl.setopt(pycurl.HEADERFUNCTION, header.write)
+    curl.setopt(pycurl.WRITEFUNCTION, response.write)
+    curl.setopt(pycurl.COOKIEJAR, 'cookie.txt')
+    curl.setopt(pycurl.COOKIEFILE, 'cookie.txt')
+    curl.perform()
+    curl.close()
+    print('Get function end')
+
+def urlpost(header, response):
+    print('Post function start')
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.POST, 1)
+    curl.setopt(pycurl.URL, "http://www.shortstoryproject.com/wp-content/themes/maaboret2016/action.php")
+    curl.setopt(pycurl.USERAGENT, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+    curl.setopt(pycurl.HTTPHEADER, ["Connection: keep-alive", "Expect:"])
+    curl.setopt(pycurl.SSL_VERIFYPEER, 0)
+    curl.setopt(pycurl.SSL_VERIFYHOST, 0)
+    curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
+    curl.setopt(pycurl.HEADERFUNCTION, header.write)
+    curl.setopt(pycurl.WRITEFUNCTION, response.write)
+    curl.setopt(pycurl.COOKIEJAR, 'cookie.txt')
+    curl.setopt(pycurl.COOKIEFILE, 'cookie.txt')
+    curl.setopt(pycurl.HTTPPOST, [("posts", "1000000"), ("offset", "0"), ("lang", "he")])
+    curl.perform()
+    curl.close()
+    print('Post function end')
+
+
+def main():
+    header = BytesIO()
+    response = BytesIO()
+    print('Get main page & cookie')
+    urlget(header, response)
+    for line in header.getvalue().splitlines():
+        print(line.decode())
+    str_response = response.getvalue().decode()
+    print(str_response)
+
+    print()
+    print()
+
+    header = BytesIO()
+    response = BytesIO()
+    print('Post library page loader')
+    urlpost(header, response)
+    for line in header.getvalue().splitlines():
+        print(line.decode())
+    str_response = response.getvalue()
+    with open('output.txt', 'wb') as file:
+        file.write(str_response)
+
+
+if __name__ == '__main__':
+    main()
